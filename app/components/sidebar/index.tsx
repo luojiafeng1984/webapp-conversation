0516dc21433bc5bd36d03c2abe4e3b2ff9cb3f15
luojiafeng <luojiafeng@cnstrong.cn>
2024-05-22 16:54:26 +0800

    内部聊天工具版权信息修改

--- app/components/sidebar/index.tsx
+++ app/components/sidebar/index.tsx
@@ -79,7 +79,6 @@ const Sidebar: FC<ISidebarProps> = ({
       </a> */}
       <div className="flex flex-shrink-0 pr-4 pb-4 pl-4">
         <div className="text-gray-400 font-normal text-xs">© {copyRight} {(new Date()).getFullYear()}</div>
-        <div className="text-gray-400 font-normal text-xs">© {copyRight} {(new Date()).getFullYear()}</div>
       </div>
     </div>
   )
